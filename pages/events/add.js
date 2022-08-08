a9244b8cb27712a6548ecd063fa230aee3783c67
Sung Kil Han <90734610+greatandrewhan@users.noreply.github.com>
2022-08-07 23:39:15 -0400

    Layout, Header and Footer

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,3 +1,9 @@
+import Layout from "../../components/Layout";
+
 export default function AppEventPage() {
-  return <div>Add Event</div>;
+  return (
+    <Layout title="Add New Event">
+      <h1>Add Event</h1>
+    </Layout>
+  );
 }
